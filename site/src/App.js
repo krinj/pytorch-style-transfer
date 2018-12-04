fdcd3e1e363f7c14b733191a96d3b23ec5c279cc
Jakrin Juangbhanich <juangbhanich.k@gmail.com>
2018-12-04 21:24:26 +0800

    Added Region Update

--- site/src/App.js
+++ site/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-			  Hello World!
+			  Hello World 222!
 			  Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
