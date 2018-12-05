aea772c2c7548f85f23144b916400eee1b3b6ef8
Jakrin Juangbhanich <juangbhanich.k@gmail.com>
2018-12-06 00:42:41 +0800

    Hooked up to Lambda API

--- site/src/App.js
+++ site/src/App.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./custom.css"
 import Viewer from "./Viewer";
+
 
 class App extends Component {
 	render() {
--- site/src/Viewer.js
+++ site/src/Viewer.js
@@ -1,7 +1,5 @@
 import React, {Component} from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'bootstrap/dist/css/bootstrap-theme.min.css';
-
+import axios from "axios";
 
 class Viewer extends Component {
 
@@ -9,10 +7,9 @@ class Viewer extends Component {
 		super();
 		this.state = {
 			value: 0,
-			message: "Hello World"
+			message: "Hello World!!"
 		}
 	}
-
 
 	doSomething = () => {
 		console.log(`DoSomething ${this.state.value}`);
@@ -23,19 +20,40 @@ class Viewer extends Component {
 		this.setState(newState);
 	};
 
+	getResponse = (res) => {
+		console.log(res);
+		console.log(res.data);
+		console.log(`Got Response ${res}`);
+		const newState = {...this.state};
+		newState.value = res.data.k2;
+		newState.message = res.data.k1;
+		this.setState(newState);
+	};
+
+	sendRequest = () => {
+		let myData= {key1: 1000};
+		// let config = {
+		// 	headers: {
+		// 		'Content-Type': 'application/json',
+		// 	}
+		// };
+		axios.post(`https://xliyrr4cp4.execute-api.ap-southeast-2.amazonaws.com/default/requestStyleTransfer`, myData)
+			.then(this.getResponse)
+	};
+
 	render() {
 		return (
 			<div className="container theme-showcase" role="main">
 
 				<div>Hello</div>
-				<div className="jumbotron">
+				<div className="jumbotron bg-dark">
 					<h1>Theme example</h1>
 				</div>
 
 				<p>Template Runner</p>
 				<p>{this.state.message}</p>
 				<p>{`Clicks: ${this.state.value}`}</p>
-				<button type="button" className="btn btn-primary" onClick={this.doSomething}>Primary</button>
+				<button type="button" className="btn btn-primary" onClick={this.sendRequest}>Primary</button>
 
 			</div>
 			// <div>
